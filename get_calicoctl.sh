ded98321e7d3a1b851ed65a06d9d5fd608552344
sagarakolkar <98311626+sagarakolkar@users.noreply.github.com>
2025-01-22 23:40:39 +0530

    Update get_calicoctl.sh

--- get_calicoctl.sh
+++ get_calicoctl.sh
@@ -1,2 +1,4 @@
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.29.1/manifests/calico-vxlan.yaml
+kubectl -n kube-system set env daemonset/calico-node FELIX_AWSSRCDSTCHECK=Disable
 curl -L https://github.com/projectcalico/calico/releases/download/v3.29.1/calicoctl-linux-amd64 -o calicoctl
 chmod +x calicoctl
