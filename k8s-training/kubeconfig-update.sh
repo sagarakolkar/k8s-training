5a669f0bc3cf1a4e09ea183bceceb99713db9b57
sagarakolkar <98311626+sagarakolkar@users.noreply.github.com>
2025-01-23 13:32:16 +0530

    Update kubeconfig-update.sh

--- k8s-training/kubeconfig-update.sh
+++ k8s-training/kubeconfig-update.sh
@@ -1,2 +1,2 @@
-aws eks update-kubeconfig --region us-east-1 --name calico-training
+aws eks update-kubeconfig --region us-east-1 --name k8s-training
 aws eks update-kubeconfig --region us-east-1 --name cilium-training
