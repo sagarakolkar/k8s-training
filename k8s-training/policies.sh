d4bde7890bdf00b4958e8dab92337fe293037d25
sagarakolkar <98311626+sagarakolkar@users.noreply.github.com>
2025-01-30 12:42:22 +0530

    Update policies.sh

--- k8s-training/policies.sh
+++ k8s-training/policies.sh
@@ -1,6 +1,18 @@
+# No Policies
 kubectl exec -it busybox -- sh
 wget -qO- http://nginx-app2.default.svc.cluster.local
 
+# Block all ingress
 kubectl apply -f normal-deny-all-ingress.yaml
 kubectl exec -it busybox -- sh
 wget -qO- http://nginx-app2.default.svc.cluster.local
+
+# Allow ingress from same namespace
+normal-all-busybox-ingress.yaml
+kubectl exec -it busybox -- sh
+
+# Allow ingress from pods in same namespace with specific label
+kubectl apply -f normal-busybox-with-nginx-label.yaml 
+kubectl exec -it busybox -- sh
+kubectl apply -f busybox-nginx.yaml
+kubectl exec -it busybox-nginx -- sh
