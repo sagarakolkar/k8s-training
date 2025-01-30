22e1fa911d4acdee2da478251e581b8e47ecd6ae
sagarakolkar <98311626+sagarakolkar@users.noreply.github.com>
2025-01-30 13:09:13 +0530

    Update policies.sh

--- k8s-training/policies.sh
+++ k8s-training/policies.sh
@@ -16,3 +16,22 @@ kubectl apply -f normal-busybox-with-nginx-label.yaml
 kubectl exec -it busybox -- sh
 kubectl apply -f busybox-nginx.yaml
 kubectl exec -it busybox-nginx -- sh
+
+
+# Download calicoctl
+curl -L https://github.com/projectcalico/calico/releases/download/v3.29.1/calicoctl-linux-amd64 -o calicoctl
+chmod +x ./calicoctl
+
+#Apply calico global policy 
+./calicoctl apply -f calico-global-policy.yaml
+kubectl exec -it busybox-nginx -- sh
+wget -qO- http://nginx-app1.app1.svc.cluster.local
+
+#Apply calico global policy - allow few ns
+kubectl apply -f calico-global-allow-few-ns.yaml
+kubectl exec -it busybox-nginx -- sh
+wget -qO- http://nginx-app1.app1.svc.cluster.local
+
+kubectl label namespace default allowed=nginx
+kubectl exec -it busybox-nginx -- sh
+wget -qO- http://nginx-app1.app1.svc.cluster.local
